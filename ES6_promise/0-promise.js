ca9c0b6d12ae4cfd6cb40cec63a18e8c6dee8c75
MarlenaKule <marlenakule1@gmail.com>
2026-08-16 14:39:57 +0200

    task_0

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,5 +1,6 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject))=> {
-        resolve('');
-    });
+  return new Promise((resolve, reject) => {
+    resolve('');
+    reject(new Error('Something bad happen!'));
+  });
 }
