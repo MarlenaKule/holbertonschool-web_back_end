2b090114173df821b9cc985b9d4ef5416d633ccc
MarlenaKule <marlenakule1@gmail.com>
2026-09-18 18:40:21 +0200

    task 0

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,4 @@
--- write a SQL script that creates a table users
+-- Creates a table 'users' with id, email, and name attributes
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
